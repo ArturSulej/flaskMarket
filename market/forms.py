ebce7590f1f197b6b687d16f30f81109e3e3a671
ArturSulej <sulejartur2@gmail.com>
2022-07-16 14:18:42 +0200

    End of section P10 of the tutorial

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username')
-    email_address = StringField(label='Email Address')
-    password = PasswordField(label='Password')
-    password_confirm = PasswordField(label='Confirm Password')
+
+    def validate_username(self, username_to_check):     # Flask form automatically find functions starting with word validate, and executes them on coresponding fields
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists! Please try a different username')
+
+    def validate_email_address(self, email_address_to_check):     # Flask form automatically find functions starting with word validate, and executes them on coresponding fields
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email address already taken! Please try a different email')
+
+    username = StringField(label='Username', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
+    password_confirm = PasswordField(label='Confirm Password', validators=[EqualTo('password'), DataRequired()])
     submit = SubmitField(label='Create Account')
