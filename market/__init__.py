23139478126a2b1b24f5014297e8f78149719f72
ArturSulej <sulejartur2@gmail.com>
2022-07-18 12:45:45 +0200

    End of section P12 of the tutorial

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)  # Initialize Flask, get local python file name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'   # Add extra configuration to allow app locate database
 app.config['SECRET_KEY'] = '838cf2d042d04baf3eda70a6'
 db = SQLAlchemy(app)   # Instance of SQLAlchemy class
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
--- market/models.py
+++ market/models.py
@@ -1,6 +1,11 @@
-from market import db, bcrypt
+from market import db, bcrypt, login_manager
+from flask_login import UserMixin # Required methods for user auth
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), unique=True, nullable=False)
     email_address = db.Column(db.String(length=50), unique=True, nullable=False)
@@ -9,12 +14,23 @@ class User(db.Model):
     items = db.relationship('Item', backref='owned_user', lazy=True)   # Relationship with item, backref - access items through user and get which user owns specific item, lazy - grab all items at once
 
     @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+        else:
+            return f'{self.budget}$'
+
+    @property
     def password(self):
         return self.password
 
     @password.setter
     def password(self, plain_text_password):    # Executed while creating user
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
